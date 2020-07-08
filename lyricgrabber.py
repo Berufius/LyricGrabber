ff38ed81172c3806da8c03436556af0647aeacfc
Berufius <benjamin.jongenburger@gmail.com>
2020-07-08 12:05:39 +0200

    Print lyrics in seperate function, incl. check if return = None

--- lyricgrabber.py
+++ lyricgrabber.py
@@ -11,39 +11,47 @@ def main():
     # Create spotify object
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,client_secret=CLIENT_SECRET,
                                                    redirect_uri=REDIRECT_URI,scope=scope,username=USER))
-    # Genius object and properties
-    genius = lyricsgenius.Genius(GENIUS_TOKEN)
-    genius.skip_non_songs = True
-    genius.excluded_terms = ["(Remix)", "(Live)", "(Instrumental)"]
     # Set track name to nothing
-    track_name = ""
-
+    track = ""
     while True:
         # Receive Spotify response
         results = sp.current_user_playing_track()
-        if not results == None:
+        if results != None:
             # Check if same song as previous iteration still plays
-            if not track_name == results['item']['name']:
+            if not track == results['item']['name']:
                 artist = results['item']['artists'][0]['name']
                 album = results['item']['album']['name']
-                track_name = results['item']['name']
-                # print summary
-                song = genius.search_song(title=track_name, artist=artist)
-                # Check if genius return values correspond with spotipy
-                if (song != None and str(song.artist).lower() == artist.lower()
-                    and str(song.title).lower() == track_name.lower()):
-                    print(f"\nArtist: {artist}")
-                    print(f"Album: {album}")
-                    print(f"Track name: {track_name}\n")
-                    print(song.lyrics)
-                    print()
-                else:
-                    print(f"There are no lyrics available for:\n{artist} - {track_name}")
+                track = results['item']['name']
+                # Print lyrics
+                print_lyrics(artist, track, album)
         else:
             # Nothing is playing, quitting script
             print("Nothing is playing, exit script...")
             return
         sleep(10)
+
+
+def print_lyrics(artist, track, album):
+    """ Searches song lyrics in genius db and prints them
+
+    Args:
+        artist (string): artist name
+        track (string): track name
+        album (string): album name
+    """
+    # Genius object and properties
+    genius = lyricsgenius.Genius(GENIUS_TOKEN)
+    genius.skip_non_songs = True
+    genius.excluded_terms = ["(Remix)", "(Live)", "(Instrumental)"]
+    # Request genius object
+    song = genius.search_song(title=track, artist=artist)
+    # Check if genius object corresponds with spotify object parameters
+    if (song != None and str(song.artist).lower() == artist.lower()):
+        # Print lyrics and song parameters
+        print(f"\nArtist: {artist}\nAlbum: {album}\nTrack name: {track}\n{song.lyrics}\n")
+    else:
+        print(f"There are no lyrics available for:\n{artist} - {track}")
+    return
 
 
 if __name__ == "__main__":
